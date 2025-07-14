59a2b4fa8bf1f95b311016ecb682d708c8bb05c0
Ulad Kasach <uladkasach@gmail.com>
2025-07-14 13:00:52 -0500

    fix(pkg): export template, thread, role procedures

--- src/contract/sdk.ts
+++ src/contract/sdk.ts
@@ -1,3 +1,5 @@
+export * from '../domain/objects';
+
 export { genStitchChoice } from '../logic/weave/compose/genStitchChoice';
 export { genStitchCycle } from '../logic/weave/compose/genStitchCycle';
 export { genStitchFanout } from '../logic/weave/compose/genStitchFanout';
@@ -8,4 +10,12 @@ export { enweaveOneStitcher } from '../logic/weave/enweaveOneStitcher';
 export { asStitcher } from '../logic/weave/compose/asStitcher';
 export { asStitcherFlat } from '../logic/weave/compose/asStitcherFlat';
 
-export * from '../domain/objects';
+export { genTemplate } from '../logic/template/genTemplate';
+export { getTemplateValFromArtifacts } from '../logic/template/getTemplateValFromArtifacts';
+export { getTemplateVarsFromRoleInherit } from '../logic/template/getTemplateVarsFromInheritance';
+export { useTemplate } from '../logic/template/useTemplate';
+export { genStepImagineViaTemplate } from '../logic/template/genStepImagineViaTemplate';
+
+export { ContextStitchTrail } from '../logic/stitch/withStitchTrail';
+export { getStitch } from '../logic/thread/getStitch';
+export { enrollThread } from '../logic/role/enrollThread';
